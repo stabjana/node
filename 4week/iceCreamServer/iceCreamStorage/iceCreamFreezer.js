b686dae5363d08acab24745baf55c714bce9b4fb
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-11-14 12:45:17 +0200

    add getAllFlavours function

---
+++ 4week/iceCreamServer/iceCreamStorage/iceCreamFreezer.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const path = require('path'); // dont need mimetypes, data is only used in utilities function
+
+const { read } = require('../library/utilities'); // here we access the ice Cream with 3 fts
+// not nice hard Coded part
+//the utilities.js reads and handles the mimetypes, we can use it everywhere now and change it however we like
+const jsonPath = path.join(__dirname, 'iceCream.json');
+
+async function getAllFlavours() {
+    try {
+        const data = await read(jsonPath);
+        const iceCream = await JSON.parse(data.fileData);
+
+        return Object.keys(iceCream);
+
+    } catch (error) {
+        return [];
+    }
+}
+module.exports = { getAllFlavours }
